8d04dcbc434e60e1dd07d05fefe361d67049193b
Dan DeGenaro <daniel.degenaro@gmail.com>
2026-03-07 17:57:29 -0500

    pushing

--- find_nan.py
+++ find_nan.py
@@ -4,5 +4,6 @@ import numpy as np
 
 for log_tsv in glob.glob('experiments/*/*/metrics.tsv'):
     df = pd.read_csv(log_tsv, sep='\t')
-    if df['ce'].isna().any():
-        print(log_tsv)
+    s = df['ce'].isna().sum()
+    if s > 0:
+        print(log_tsv, s)
