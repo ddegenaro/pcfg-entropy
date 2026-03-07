a68860e4102ff5c9165fe61ef49a11899c80b02b
Dan DeGenaro <daniel.degenaro@gmail.com>
2026-03-07 17:53:53 -0500

    ugh

---
+++ find_nan.py
@@ -0,0 +1,8 @@
+import glob
+import pandas as pd
+import numpy as np
+
+for log_tsv in glob.glob('experiments/*/*/metrics.tsv'):
+    df = pd.read_csv(log_tsv, sep='\t')
+    if np.nan in df['ce']:
+        print(log_tsv)
