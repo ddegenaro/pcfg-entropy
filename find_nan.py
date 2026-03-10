da32e476dc0ebf63db086425ce5be6ad12e4507f
Dan DeGenaro <daniel.degenaro@gmail.com>
2026-03-10 13:17:08 -0400

    nan search

--- find_nan.py
+++ find_nan.py
@@ -10,7 +10,7 @@ for log_tsv in files:
     df = pd.read_csv(log_tsv, sep='\t')
     s = df['ce'].isna().sum()
     if s > 0:
-        print(log_tsv, f'{100 * s / len(df):d}')
+        print(log_tsv, f'{round(100 * s / len(df))}')
         c += 1
         
 print(f'nan found in {c}/{len(files)} logs')
