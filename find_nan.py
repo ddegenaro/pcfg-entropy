011cc2a5c142d0d69439ec513992d681bb5ce334
Dan DeGenaro <daniel.degenaro@gmail.com>
2026-03-10 13:21:14 -0400

    nan search

--- find_nan.py
+++ find_nan.py
@@ -11,7 +11,13 @@ for log_tsv in files:
     s = df['ce'].isna().sum()
     if s > 0:
         j = json.load(open(log_tsv.replace('metrics.tsv', 'hparams.json')))
-        print(log_tsv, f'var={j["var"]}', f'{round(100 * s / len(df))}')
+        var = j['var']
+        arch = 'lstm' if 'lstm' in log_tsv else 'trf'
+        grammar_type = j['grammar_type']
+        num_symbols = j['grammar_num_symbols']
+        nts = j['grammar_formalism_arg']
+        seed = j['grammar_seed']
+        print(arch, grammar_type, var, num_symbols, nts, seed)
         c += 1
         
 print(f'nan found in {c}/{len(files)} logs')
